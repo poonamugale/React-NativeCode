6ebe27a1dcb5e1102333da8db521135733fb6d9c
Poonam <poonamugale1997@gmail.com>
2024-11-15 15:25:27 +0530

    Functional Components

--- App.jsx
+++ App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Text, View, Button} from 'react-native';
-
+import First from './src/components/First';
 const App = () => {
   return (
     <View>
       <Text style={{fontSize: 30}}>React Native</Text>
       <Text style={{fontSize: 30}}>First Progrm</Text>
       <Button title="Press here" />
+      <First />
     </View>
   );
 };
---
+++ src/components/First.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+// Functional Componets
+const First = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>Functional Component</Text>
+    </View>
+  );
+};
+
+export default First;
