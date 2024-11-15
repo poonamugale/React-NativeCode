cf3cd374533bfaa115ef48d6d217cec46f5fbf4d
Poonam <poonamugale1997@gmail.com>
2024-11-15 14:55:13 +0530

    View,Text,Button

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, Button} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>React Native</Text>
+      <Text style={{fontSize: 30}}>First Progrm</Text>
+      <Button title="Press here" />
+    </View>
+  );
+};
+
+export default App;
